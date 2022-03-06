84f0f1d2a4bd67eb373b8cff7e659831fd746a8a
AYAN SIDDIQUI <52853276+ayansiddiqui007@users.noreply.github.com>
2022-03-06 23:57:36 +0530

    1

---
+++ index.js
@@ -0,0 +1,16 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+const url = 'https://www.theguardian.com/international'
+
+axios(url)
+     .then(response => {
+         const html = response.data
+         console.log(html)
+     })
+
+app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
